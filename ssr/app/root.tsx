ac7665c02575af35ab44b59e15e79a2bfbe00da1
Jokoson <exampleexampleexample12135example@example.com>
2025-03-17 15:45:04 +0100

    add lucide-react dependency and implement ArrowSvg and ChevronDownIcon components in Layout

--- ssr/app/root.tsx
+++ ssr/app/root.tsx
@@ -1,127 +1,127 @@
-import {
-  isRouteErrorResponse,
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-} from "react-router";
+import {isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData,} from "react-router";
 
-import type { Route } from "./+types/root";
+import type {Route} from "./+types/root";
 import "./app.css";
 import {getSettingsData} from "~/api/strapi-api";
 import React from "react";
-import { Menu } from "@base-ui-components/react";
+import {Menu} from "@base-ui-components/react";
+import {AtomIcon} from "lucide-react";
 
 export async function loader() {
-  try {
-    return await getSettingsData();
-  } catch (error) {
-    console.error(error);
-    return { data: null };
-  }
+    try {
+        return await getSettingsData();
+    } catch (error) {
+        console.error(error);
+        return {data: null};
+    }
 }
 
-export function Layout({ children }: { children: React.ReactNode }) {
-  const loaderData: any = useLoaderData<Route.ComponentProps>(); // Use the hook to access loaderData
-  const menuItem = loaderData.top
+export function Layout({children}: { children: React.ReactNode }) {
+    const loaderData: any = useLoaderData<Route.ComponentProps>(); // Use the hook to access loaderData
+    const menuItem = loaderData.top
 
-  return (
-      <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-      {menuItem.map((item: any) => (
-          <Menu.Root key={item.id} openOnHover>
-            <Menu.Trigger className="flex h-10 items-center justify-center gap-1.5 rounded-md border border-gray-200 bg-gray-50 px-3.5 text-base font-medium text-gray-900 select-none hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-100 data-[popup-open]:bg-gray-100">
-              {item.label} <ChevronDownIcon className="-mr-1" />
-            </Menu.Trigger>
-            <Menu.Portal>
-              <Menu.Positioner className="outline-none" sideOffset={8}>
-                <Menu.Popup className="origin-[var(--transform-origin)] rounded-md bg-[canvas] py-1 text-gray-900 shadow-lg shadow-gray-200 outline outline-1 outline-gray-200 transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 dark:shadow-none dark:-outline-offset-1 dark:outline-gray-300">
-                  <Menu.Arrow className="data-[side=bottom]:top-[-8px] data-[side=left]:right-[-13px] data-[side=left]:rotate-90 data-[side=right]:left-[-13px] data-[side=right]:-rotate-90 data-[side=top]:bottom-[-8px] data-[side=top]:rotate-180">
-                    <ArrowSvg />
-                  </Menu.Arrow>
-                  {item.children.map((child: any, index: string | number) => (
-                      <Menu.Item key={index} className="flex cursor-default py-2 pr-8 pl-4 text-sm leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900">
-                        {child.label}
-                      </Menu.Item>
-                  ))}
-                </Menu.Popup>
-              </Menu.Positioner>
-            </Menu.Portal>
-          </Menu.Root>
-      ))}
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <Meta/>
+            <Links/>
+        </head>
+        <body>
+        <div className="container mx-auto flex gap-5">
+            {menuItem.map((item: any) => (
+                <Menu.Root key={item.id} openOnHover>
+                    <Menu.Trigger
+                        className="flex h-10 items-center justify-center gap-1.5 rounded-md px-3.5 text-base font-medium text-gray-900 select-none hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-100 data-[popup-open]:bg-gray-100">
+                        {item.label}
+                        <ChevronDownIcon className="-mr-1"/>
+                    </Menu.Trigger>
+                    <Menu.Portal>
+                        <Menu.Positioner className="outline-none" sideOffset={8}>
+                            <Menu.Popup
+                                className="origin-[var(--transform-origin)] rounded-md bg-[canvas] py-1 text-gray-900 shadow-lg shadow-gray-200 outline outline-1 outline-gray-200 transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 dark:shadow-none dark:-outline-offset-1 dark:outline-gray-300">
+                                <Menu.Arrow
+                                    className="data-[side=bottom]:top-[-8px] data-[side=left]:right-[-13px] data-[side=left]:rotate-90 data-[side=right]:left-[-13px] data-[side=right]:-rotate-90 data-[side=top]:bottom-[-8px] data-[side=top]:rotate-180">
+                                    <ArrowSvg/>
+                                </Menu.Arrow>
+                                {item.children.map((child: any, index: string | number) => (
+                                    <Menu.Item key={index}
+                                               className="flex cursor-default py-2 pr-8 pl-4 text-sm leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900">
+                                        {child.label}
+                                    </Menu.Item>
+                                ))}
+                            </Menu.Popup>
+                        </Menu.Positioner>
+                    </Menu.Portal>
+                </Menu.Root>
+            ))}
+        </div>
         {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+        <ScrollRestoration/>
+        <Scripts/>
+        </body>
+        </html>
+    );
 }
 
 export default function App() {
-  return <Outlet />;
+    return <Outlet/>;
 }
 
-export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
-  let stack: string | undefined;
+export function ErrorBoundary({error}: Route.ErrorBoundaryProps) {
+    let message = "Oops!";
+    let details = "An unexpected error occurred.";
+    let stack: string | undefined;
 
-  if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
-    details =
-      error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
-    stack = error.stack;
-  }
+    if (isRouteErrorResponse(error)) {
+        message = error.status === 404 ? "404" : "Error";
+        details =
+            error.status === 404
+                ? "The requested page could not be found."
+                : error.statusText || details;
+    } else if (import.meta.env.DEV && error && error instanceof Error) {
+        details = error.message;
+        stack = error.stack;
+    }
 
-  return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
+    return (
+        <main className="pt-16 p-4 container mx-auto">
+            <h1>{message}</h1>
+            <p>{details}</p>
+            {stack && (
+                <pre className="w-full p-4 overflow-x-auto">
           <code>{stack}</code>
         </pre>
-      )}
-    </main>
-  );
+            )}
+        </main>
+    );
 }
 
 
 function ArrowSvg(props: React.ComponentProps<'svg'>) {
-  return (
-      <svg width="20" height="10" viewBox="0 0 20 10" fill="none" {...props}>
-        <path
-            d="M9.66437 2.60207L4.80758 6.97318C4.07308 7.63423 3.11989 8 2.13172 8H0V10H20V8H18.5349C17.5468 8 16.5936 7.63423 15.8591 6.97318L11.0023 2.60207C10.622 2.2598 10.0447 2.25979 9.66437 2.60207Z"
-            className="fill-[canvas]"
-        />
-        <path
-            d="M8.99542 1.85876C9.75604 1.17425 10.9106 1.17422 11.6713 1.85878L16.5281 6.22989C17.0789 6.72568 17.7938 7.00001 18.5349 7.00001L15.89 7L11.0023 2.60207C10.622 2.2598 10.0447 2.2598 9.66436 2.60207L4.77734 7L2.13171 7.00001C2.87284 7.00001 3.58774 6.72568 4.13861 6.22989L8.99542 1.85876Z"
-            className="fill-gray-200 dark:fill-none"
-        />
-        <path
-            d="M10.3333 3.34539L5.47654 7.71648C4.55842 8.54279 3.36693 9 2.13172 9H0V8H2.13172C3.11989 8 4.07308 7.63423 4.80758 6.97318L9.66437 2.60207C10.0447 2.25979 10.622 2.2598 11.0023 2.60207L15.8591 6.97318C16.5936 7.63423 17.5468 8 18.5349 8H20V9H18.5349C17.2998 9 16.1083 8.54278 15.1901 7.71648L10.3333 3.34539Z"
-            className="dark:fill-gray-300"
-        />
-      </svg>
-  );
+    return (
+        <svg width="20" height="10" viewBox="0 0 20 10" fill="none" {...props}>
+            <path
+                d="M9.66437 2.60207L4.80758 6.97318C4.07308 7.63423 3.11989 8 2.13172 8H0V10H20V8H18.5349C17.5468 8 16.5936 7.63423 15.8591 6.97318L11.0023 2.60207C10.622 2.2598 10.0447 2.25979 9.66437 2.60207Z"
+                className="fill-[canvas]"
+            />
+            <path
+                d="M8.99542 1.85876C9.75604 1.17425 10.9106 1.17422 11.6713 1.85878L16.5281 6.22989C17.0789 6.72568 17.7938 7.00001 18.5349 7.00001L15.89 7L11.0023 2.60207C10.622 2.2598 10.0447 2.2598 9.66436 2.60207L4.77734 7L2.13171 7.00001C2.87284 7.00001 3.58774 6.72568 4.13861 6.22989L8.99542 1.85876Z"
+                className="fill-gray-200 dark:fill-none"
+            />
+            <path
+                d="M10.3333 3.34539L5.47654 7.71648C4.55842 8.54279 3.36693 9 2.13172 9H0V8H2.13172C3.11989 8 4.07308 7.63423 4.80758 6.97318L9.66437 2.60207C10.0447 2.25979 10.622 2.2598 11.0023 2.60207L15.8591 6.97318C16.5936 7.63423 17.5468 8 18.5349 8H20V9H18.5349C17.2998 9 16.1083 8.54278 15.1901 7.71648L10.3333 3.34539Z"
+                className="dark:fill-gray-300"
+            />
+        </svg>
+    );
 }
 
 function ChevronDownIcon(props: React.ComponentProps<'svg'>) {
-  return (
-      <svg width="10" height="10" viewBox="0 0 10 10" fill="none" {...props}>
-        <path d="M1 3.5L5 7.5L9 3.5" stroke="currentcolor" strokeWidth="1.5" />
-      </svg>
-  );
+    return (
+        <svg width="10" height="10" viewBox="0 0 10 10" fill="none" {...props}>
+            <path d="M1 3.5L5 7.5L9 3.5" stroke="currentcolor" strokeWidth="1.5"/>
+        </svg>
+    );
 }
