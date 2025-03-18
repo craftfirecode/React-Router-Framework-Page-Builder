43a7b6ca0cab19f11e3e173cdd79e5760919a75b
Jokoson <exampleexampleexample12135example@example.com>
2025-03-18 07:28:45 +0100

    feat: add Navigation component for dynamic menu rendering and refactor layout

--- ssr/app/root.tsx
+++ ssr/app/root.tsx
@@ -1,8 +1,7 @@
 import {
     isRouteErrorResponse,
-    Link,
     Links,
-    Meta, NavLink,
+    Meta,
     Outlet,
     Scripts,
     ScrollRestoration,
@@ -11,11 +10,8 @@ import {
 import type {Route} from "./+types/root";
 import "./app.css";
 import React from "react";
-import {Menu} from "@base-ui-components/react";
-import * as Icons from "lucide-react";
-import {useLocation} from "react-router";
 import {getSettingsData} from "~/api/strapi-api";
-
+import {Navigation} from "~/components/ui/navigation";
 
 export async function loader() {
     try {
@@ -26,21 +22,9 @@ export async function loader() {
     }
 }
 
-function DynamicIcon({iconName}: { iconName: string }) {
-    // @ts-ignore
-    const IconComponent: any = Icons[iconName];
-    return IconComponent ? <IconComponent/> : null;
-}
-
-
 export function Layout({children}: { children: React.ReactNode }) {
-    const location = useLocation();
     const loaderData: any = useLoaderData<Route.ComponentProps>();
-    const menuItem = loaderData.top;
 
-    const isActive = (path: string): boolean => {
-        return location.pathname.startsWith('/' + path);
-    }
     return (
         <html lang="en">
         <head>
@@ -50,79 +34,7 @@ export function Layout({children}: { children: React.ReactNode }) {
             <Links/>
         </head>
         <body className="dark">
-        <nav className="container mx-auto flex items-center gap-2 pt-5">
-            {menuItem.map((item: any) => (
-                <React.Fragment key={item.id}>
-                    {item.children.length === 0 ? (
-                        <NavLink
-                            caseSensitive
-                            className={({isActive}) =>
-                                isActive
-                                    ? "text-[#00c16a] bg-[#1d293d] rounded-md py-2 px-3"
-                                    : "text-[#62748e] py-2 px-3 transition-colors duration-450 hover:text-[#00c16a]"
-                            }
-                            to={item.to}
-                        >
-                            <div className="flex items-center gap-1.5">
-                                <DynamicIcon iconName={item.icon}/>
-                                {item.label}
-                            </div>
-                        </NavLink>
-                    ) : (
-                        <Menu.Root openOnHover>
-                            {(() => {
-                                const hasActiveChild = item.children.some((child: any) =>
-                                    isActive(child.to)
-                                );
-
-                                return (
-                                    <>
-                                        <Menu.Trigger
-                                            className={`flex h-10 items-center justify-center gap-1.5 rounded-md px-3.5 select-none focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-100 data-[popup-open]:text-[#00c16a] ${
-                                                hasActiveChild ? "text-[#00c16a] bg-[#1d293d]" : "text-[#62748e] py-2 px-3 transition-colors duration-450"
-                                            }`}
-                                        >
-                                            <DynamicIcon iconName={item.icon}/>
-                                            {item.label}
-                                            <ChevronDownIcon className="-mr-1"/>
-                                        </Menu.Trigger>
-                                        <Menu.Portal>
-                                            <Menu.Positioner className="outline-none" sideOffset={8}>
-                                                <Menu.Popup
-                                                    className="origin-[var(--transform-origin)] rounded-md bg-[canvas] py-1 text-gray-900 shadow-lg shadow-gray-200 outline outline-1 outline-gray-200 transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 dark:shadow-none dark:-outline-offset-1 dark:outline-gray-300">
-                                                    <Menu.Arrow
-                                                        className="data-[side=bottom]:top-[-8px] data-[side=left]:right-[-13px] data-[side=left]:rotate-90 data-[side=right]:left-[-13px] data-[side=right]:-rotate-90 data-[side=top]:bottom-[-8px] data-[side=top]:rotate-180">
-                                                        <ArrowSvg/>
-                                                    </Menu.Arrow>
-                                                    {item.children.map((child: any, index: number) => (
-                                                        <NavLink
-                                                            caseSensitive
-                                                            key={index}
-                                                            className={({isActive}) =>
-                                                                isActive
-                                                                    ? "app-nav-link text-[#00c16a] bg-[#f1f5f9]"
-                                                                    : "app-nav-link text-[#62748e] hover:text-black"
-                                                            }
-                                                            to={child.to}
-                                                        >
-                                                            <Menu.Item
-                                                                className="flex items-center gap-1.5 cursor-default py-2 pr-8 pl-4 text-sm leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900">
-                                                                <DynamicIcon iconName={child.icon}/>
-                                                                {child.label}
-                                                            </Menu.Item>
-                                                        </NavLink>
-                                                    ))}
-                                                </Menu.Popup>
-                                            </Menu.Positioner>
-                                        </Menu.Portal>
-                                    </>
-                                );
-                            })()}
-                        </Menu.Root>
-                    )}
-                </React.Fragment>
-            ))}
-        </nav>
+        <Navigation data={loaderData.top} />
         {children}
         <ScrollRestoration/>
         <Scripts/>
@@ -161,32 +73,5 @@ export function ErrorBoundary({error}: Route.ErrorBoundaryProps) {
         </pre>
             )}
         </main>
-    );
-}
-
-function ArrowSvg(props: React.ComponentProps<'svg'>) {
-    return (
-        <svg width="20" height="10" viewBox="0 0 20 10" fill="none" {...props}>
-            <path
-                d="M9.66437 2.60207L4.80758 6.97318C4.07308 7.63423 3.11989 8 2.13172 8H0V10H20V8H18.5349C17.5468 8 16.5936 7.63423 15.8591 6.97318L11.0023 2.60207C10.622 2.2598 10.0447 2.25979 9.66437 2.60207Z"
-                className="fill-[canvas]"
-            />
-            <path
-                d="M8.99542 1.85876C9.75604 1.17425 10.9106 1.17422 11.6713 1.85878L16.5281 6.22989C17.0789 6.72568 17.7938 7.00001 18.5349 7.00001L15.89 7L11.0023 2.60207C10.622 2.2598 10.0447 2.2598 9.66436 2.60207L4.77734 7L2.13171 7.00001C2.87284 7.00001 3.58774 6.72568 4.13861 6.22989L8.99542 1.85876Z"
-                className="fill-gray-200 dark:fill-none"
-            />
-            <path
-                d="M10.3333 3.34539L5.47654 7.71648C4.55842 8.54279 3.36693 9 2.13172 9H0V8H2.13172C3.11989 8 4.07308 7.63423 4.80758 6.97318L9.66437 2.60207C10.0447 2.25979 10.622 2.2598 11.0023 2.60207L15.8591 6.97318C16.5936 7.63423 17.5468 8 18.5349 8H20V9H18.5349C17.2998 9 16.1083 8.54278 15.1901 7.71648L10.3333 3.34539Z"
-                className="dark:fill-gray-300"
-            />
-        </svg>
-    );
-}
-
-function ChevronDownIcon(props: React.ComponentProps<'svg'>) {
-    return (
-        <svg width="10" height="10" viewBox="0 0 10 10" fill="none" {...props}>
-            <path d="M1 3.5L5 7.5L9 3.5" stroke="currentcolor" strokeWidth="1.5"/>
-        </svg>
     );
 }
