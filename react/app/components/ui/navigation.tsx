3a3599380834b76753e98b65548f56f3b5d2d7cb
Jokoson <exampleexampleexample12135example@example.com>
2025-06-10 11:17:44 +0200

    fix: remove redundant padding classes from navigation item styles

--- react/app/components/ui/navigation.tsx
+++ react/app/components/ui/navigation.tsx
@@ -87,7 +87,7 @@ export const Navigation = ({ data }: { data: any }) => {
                             className={`flex h-10 items-center justify-center gap-1.5 rounded-md px-3.5 select-none focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-100 data-[popup-open]:text-[#00c16a] ${
                               hasActiveChild
                                 ? "text-[#00c16a] bg-[#1d293d]"
-                                : "text-white py-2 px-3 transition-colors duration-450"
+                                : "py-2 px-3 transition-colors duration-450"
                             }`}
                           >
                             <DynamicIcon iconName={item.icon} />
