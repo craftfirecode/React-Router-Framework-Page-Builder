3169d3f6097c68e8043223f18e259db96ad98e90
Jokoson <exampleexampleexample12135example@example.com>
2025-06-17 09:23:23 +0200

    style: format code for consistency and readability in button, content-image, and image components

--- react/app/components/ui/content-image.tsx
+++ react/app/components/ui/content-image.tsx
@@ -1,24 +1,29 @@
-import {Content} from "~/components/ui/content";
-import {Image} from "~/components/ui/image";
-import {Button} from "~/components/ui/button";
-import {Link} from "react-router";
+import { Content } from "~/components/ui/content";
+import { Image } from "~/components/ui/image";
+import { Button } from "~/components/ui/button";
+import { Link } from "react-router";
 
-export const ContentImage = ({data}: { data: any }) => {
-    return (
-        <div className={'grid gap-3 md:grid-cols-1 lg:grid-cols-2 items-center'}>
-            <div>
-                <Image data={data}/>
-            </div>
-            <div className={data.revert ? 'order-last' : 'md:order-last lg:order-first'}>
-                <Content data={data}/>
-                {data.button && (
-                    <div className="mt-5">
-                        <Link to={data.button.to}>
-                            <Button>{data.button.value}</Button>
-                        </Link>
-                    </div>
-                )}
-            </div>
-        </div>
-    )
-}
+export const ContentImage = ({ data }: { data: any }) => {
+  return (
+    <div className={"grid gap-3 md:grid-cols-1 lg:grid-cols-2 items-center"}>
+      <div>
+        <Image data={data} />
+      </div>
+      <div
+        className={data.revert ? "order-last" : "md:order-last lg:order-first"}
+      >
+        <Content data={data} />
+        {data.button && (
+          <div className="mt-5">
+            <Link
+              target={data.button.blank ? "_blank" : undefined}
+              to={data.button.to}
+            >
+              <Button>{data.button.value}</Button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
--- react/app/components/ui/image.tsx
+++ react/app/components/ui/image.tsx
@@ -1,9 +1,9 @@
-export const Image = ({data}: { data: any }) => {
-    return (
-        <img
-            src={import.meta.env.VITE_PUBLIC_STRAPI_API_URL + data.image.url}
-            alt={data.image.alternativeText ? data.image.alternativeText : 'alt'}
-            className="w-full"
-        />
-    )
-}
+export const Image = ({ data }: { data: any }) => {
+  return (
+    <img
+      src={import.meta.env.VITE_PUBLIC_STRAPI_API_URL + data.image.url}
+      alt={data.image.alternativeText ? data.image.alternativeText : "alt"}
+      className="w-full"
+    />
+  );
+};
