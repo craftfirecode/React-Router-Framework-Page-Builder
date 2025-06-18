096b0fd951b5c921e91953660dc944afdc3e1ee8
Jokoson <exampleexampleexample12135example@example.com>
2025-06-18 13:41:29 +0200

    style: improve code formatting and readability in root component

--- react/app/root.tsx
+++ react/app/root.tsx
@@ -1,139 +1,138 @@
-import React, {useEffect} from "react";
+import React, { useEffect, Suspense } from "react";
 import {
-    isRouteErrorResponse,
-    Links,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration,
-    useLoaderData,
-    useLocation,
+  isRouteErrorResponse,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+  useLocation,
 } from "react-router";
-import type {Route} from "./+types/root";
+import type { Route } from "./+types/root";
 import "./app.css";
-import {getSettingsData, getStyleSettings} from "~/api/strapi-api";
-import {TopBreadcrumb} from "~/components/ui/top-breadcrumb";
-import {Footer} from "~/components/ui/footer";
-import {Navigation} from "./components/ui/navigation/navigation";
+import { getSettingsData, getStyleSettings } from "~/api/strapi-api";
+import { TopBreadcrumb } from "~/components/ui/top-breadcrumb";
+import { Footer } from "~/components/ui/footer";
+import { Navigation } from "./components/ui/navigation/navigation";
 
 export async function loader() {
-    try {
-        const [navigation, styles] = await Promise.all([
-            getSettingsData(),
-            getStyleSettings()
-        ]);
+  try {
+    const [navigation, styles] = await Promise.all([
+      getSettingsData(),
+      getStyleSettings(),
+    ]);
 
-        return {
-            navigation,
-            styles: styles || null,
-        };
-    } catch (error) {
-        console.error(error);
-        return {
-            navigation: null,
-            styles: null,
-        };
-    }
+    return {
+      navigation,
+      styles: styles || null,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      navigation: null,
+      styles: null,
+    };
+  }
 }
 
+export function Layout({ children }: { children: React.ReactNode }) {
+  const location = useLocation();
+  const loaderData: any = useLoaderData<Route.ComponentProps>();
+  const isPortfolioPage = /^\/portfolio(\/|$)/.test(location.pathname);
+  const isBlogPage = /^\/blog(\/|$)/.test(location.pathname);
 
-export function Layout({children}: { children: React.ReactNode }) {
-    const location = useLocation();
-    const loaderData: any = useLoaderData<Route.ComponentProps>();
-    const isPortfolioPage = /^\/portfolio(\/|$)/.test(location.pathname);
-    const isBlogPage = /^\/blog(\/|$)/.test(location.pathname);
-
-    useEffect(() => {
-        const checkCookieConsent = () => {
-            const consent = localStorage.getItem("cookieConsent");
-            if (!consent) {
-                // Handle case where there is no consent
-                // console.log("No cookie consent found");
-            } else {
-                // Handle case where consent is found
-                // console.log("Cookie consent found:", JSON.parse(consent));
-            }
-        };
-        checkCookieConsent();
-    }, [location]);
-
-
-    useEffect(() => {
-        if (loaderData?.styles) {
-            const root = document.documentElement;
-            console.log(loaderData.styles.body_background)
-            root.style.setProperty("--html-bg", loaderData.styles.body_background || "#000");
-        }
-    }, [loaderData]);
-
-    const onAccept = (preferences: {}) => {
-        //console.log("preferences", preferences);
+  useEffect(() => {
+    const checkCookieConsent = () => {
+      const consent = localStorage.getItem("cookieConsent");
+      if (!consent) {
+        // Handle case where there is no consent
+        // console.log("No cookie consent found");
+      } else {
+        // Handle case where consent is found
+        // console.log("Cookie consent found:", JSON.parse(consent));
+      }
     };
+    checkCookieConsent();
+  }, [location]);
 
-    return (
-        <html lang="en">
-        <head>
-            <meta charSet="utf-8"/>
-            <meta name="description" content="Website"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <Meta/>
-            <Links/>
-        </head>
-        <body className="dark h-[100vh]">
+  useEffect(() => {
+    if (loaderData?.styles) {
+      const root = document.documentElement;
+      console.log(loaderData.styles.body_background);
+      root.style.setProperty(
+        "--html-bg",
+        loaderData.styles.body_background || "#000"
+      );
+    }
+  }, [loaderData]);
+
+  const onAccept = (preferences: {}) => {
+    //console.log("preferences", preferences);
+  };
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="dark h-[100vh]">
         <div className="flex flex-col h-[100vh]">
-            <Navigation data={loaderData.navigation.top}/>
-            {isPortfolioPage && (
-                <div className="container mx-auto my-5">
-                    <TopBreadcrumb/>
-                </div>
-            )}
-            {isBlogPage && (
-                <div className="container mx-auto my-5">
-                    <TopBreadcrumb/>
-                </div>
-            )}
-            <div className="flex-1 flex flex-col">{children}</div>
-            {/*
-            <Cookie onAccept={(preferences) => onAccept(preferences)}/>
-            */}
-            <Footer/>
+          <Navigation data={loaderData.navigation.top} />
+          {isPortfolioPage && (
+            <div className="container mx-auto my-5">
+              <TopBreadcrumb />
+            </div>
+          )}
+          {isBlogPage && (
+            <div className="container mx-auto my-5">
+              <TopBreadcrumb />
+            </div>
+          )}
+          <div className="flex-1 flex flex-col">{children}</div>
+          {/* <Cookie onAccept={(preferences) => onAccept(preferences)}/> */}
+          <Footer />
         </div>
-        <ScrollRestoration/>
-        <Scripts/>
-        </body>
-        </html>
-    );
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
 
 export default function App() {
-    return <Outlet/>;
+  return <Outlet />;
 }
 
-export function ErrorBoundary({error}: Route.ErrorBoundaryProps) {
-    let message = "Oops!";
-    let details = "An unexpected error occurred.";
-    let stack: string | undefined;
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  let message = "Oops!";
+  let details = "An unexpected error occurred.";
+  let stack: string | undefined;
 
-    if (isRouteErrorResponse(error)) {
-        message = error.status === 404 ? "404" : "Error";
-        details =
-            error.status === 404
-                ? "The requested page could not be found."
-                : error.statusText || details;
-    } else if (import.meta.env.DEV && error && error instanceof Error) {
-        details = error.message;
-        stack = error.stack;
-    }
+  if (isRouteErrorResponse(error)) {
+    message = error.status === 404 ? "404" : "Error";
+    details =
+      error.status === 404
+        ? "The requested page could not be found."
+        : error.statusText || details;
+  } else if (import.meta.env.DEV && error && error instanceof Error) {
+    details = error.message;
+    stack = error.stack;
+  }
 
-    return (
-        <main className="pt-16 p-4 container mx-auto">
-            <h1>{message}</h1>
-            <p>{details}</p>
-            {stack && (
-                <pre className="w-full p-4 overflow-x-auto">
+  return (
+    <main className="pt-16 p-4 container mx-auto">
+      <h1>{message}</h1>
+      <p>{details}</p>
+      {stack && (
+        <pre className="w-full p-4 overflow-x-auto">
           <code>{stack}</code>
         </pre>
-            )}
-        </main>
-    );
+      )}
+    </main>
+  );
 }
