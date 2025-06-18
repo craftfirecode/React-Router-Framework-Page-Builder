77d57d0f11fe9df109fd938c7b294ce8d7c02b6a
Jokoson <exampleexampleexample12135example@example.com>
2025-06-18 13:36:15 +0200

    style: refactor CSS variables for improved readability and add style settings interface

---
+++ strapi/src/api/style-setting/routes/style-setting.ts
@@ -0,0 +1,7 @@
+/**
+ * style-setting router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::style-setting.style-setting');
---
+++ strapi/src/api/style-setting/services/style-setting.ts
@@ -0,0 +1,7 @@
+/**
+ * style-setting service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::style-setting.style-setting');
